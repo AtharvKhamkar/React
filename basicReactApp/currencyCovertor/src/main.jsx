b72f39db1a21937023ce9eced16ec6e9aad440f3
AtharvKhamkar <khamkaratharv2002@gmail.com>
2024-02-09 10:28:32 +0530

    add currencyConvertor project

---
+++ basicReactApp/currencyCovertor/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import NewApp from './practice/NewApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NewApp />
+  </React.StrictMode>,
+)
