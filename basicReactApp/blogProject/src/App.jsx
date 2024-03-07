0d1d49a9211bd9faf3908fd23c819e33fdd0eb8d
AtharvKhamkar <khamkaratharv2002@gmail.com>
2024-03-07 23:51:12 +0530

    configured redux store

--- basicReactApp/blogProject/src/App.jsx
+++ basicReactApp/blogProject/src/App.jsx
@@ -1,17 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { useDispatch } from "react-redux";
+import './App.css';
+import authService from "./appwrite/auth";
+import { login, logout } from './store/authSlice';
+import { Header } from './components';
 
 function App() {
-  console.log(import.meta.env.VITE_APP_APPWRITE_URL)
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({userData}))
+        } else {
+          dispatch(logout())
+        }
+      })
+    .finally(()=>setLoading(false))
+  }, [])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
 
-  return (
-    <>
-      
-      <h1>A blog app with appwrite</h1>
-    </>
-  )
+      </div>
+    </div>
+  ) : (null)
+
 }
 
 export default App
